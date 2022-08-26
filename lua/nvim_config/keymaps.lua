e10fd7f37bab3f78a2a8d6c134a0f8040cc20f8e
Emokores256 <emokores256@gmail.com>
2022-08-26 19:47:28 +0300

    changed layout for telescope

--- lua/nvim_config/keymaps.lua
+++ lua/nvim_config/keymaps.lua
@@ -67,7 +67,7 @@ keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
-keymap("n", "<C-f>", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown())<cr>", opts)
+keymap("n", "<C-f>", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes'))<cr>", opts)
 -- use the line below to shut the previewer
 --keymap("n", "<C-f>", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({previewer: false}))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
