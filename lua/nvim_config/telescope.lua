b6a95b9d81fba8cc6581d421abddba8f5d248ae1
Emokores256 <emokores256@gmail.com>
2022-08-26 18:21:44 +0300

    added pdf file preview

--- lua/nvim_config/telescope.lua
+++ lua/nvim_config/telescope.lua
@@ -92,7 +92,7 @@ telescope.setup {
     media_files = {
         -- filetypes whitelist
         -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-        filetypes = {"png", "webp", "jpg", "jpeg"},
+        filetypes = {"png", "webp", "jpg", "jpeg", "pdf"},
         find_cmd = "rg" -- find command (defaults to `fd`)
       }
     -- Your extension configuration goes here:
