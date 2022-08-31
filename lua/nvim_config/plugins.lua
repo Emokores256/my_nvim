b576a8e5adc336e7f63e4a38c39a702f767a18b3
Emokores256 <emokores256@gmail.com>
2022-08-31 11:06:30 +0300

    added new plugin lspsaga

--- lua/nvim_config/plugins.lua
+++ lua/nvim_config/plugins.lua
@@ -119,7 +119,7 @@ return packer.startup(function(use)
       },
       title = true
     }
-    
+
   end})
 
   -- jsonc file type for json - comments in json
@@ -171,6 +171,8 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use "glepnir/lspsaga.nvim"
+  -- use 'MunifTanjim/prettier.nvim'
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
