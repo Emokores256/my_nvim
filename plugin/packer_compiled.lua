1cd97943cf6a5446f22aa39ea738a3cb55895c79
Emokores256 <emokores256@gmail.com>
2022-08-31 11:06:41 +0300

    updated packer

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -177,6 +177,11 @@ _G.packer_plugins = {
     path = "/home/blackhawk/.local/share/nvim/site/pack/packer/start/lspkind-nvim",
     url = "https://github.com/onsails/lspkind-nvim"
   },
+  ["lspsaga.nvim"] = {
+    loaded = true,
+    path = "/home/blackhawk/.local/share/nvim/site/pack/packer/start/lspsaga.nvim",
+    url = "https://github.com/glepnir/lspsaga.nvim"
+  },
   ["lualine.nvim"] = {
     loaded = true,
     path = "/home/blackhawk/.local/share/nvim/site/pack/packer/start/lualine.nvim",
